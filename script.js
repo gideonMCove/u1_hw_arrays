fa59aa2313c7264fd9e9e6ce3e9c3035cc70f08b
GideonCove <gideoncove@gmail.com>
2024-04-30 17:13:17 -0400

    completed prompt 4

--- script.js
+++ script.js
@@ -49,10 +49,10 @@ Exercise 4: Access an array element
 
 Complete Exercise 4 in the space below:
 */
+let favFood = foods[1]
 
 
-
-//console.log('Exercise 4 result:', favFood);
+console.log('Exercise 4 result:', favFood);
 
 
 
